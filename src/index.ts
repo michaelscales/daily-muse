fd981594c43bc297068ed3bf496c0fc0d41337eb
michaelscales <michaelscales123@gmail.com>
2026-06-26 14:20:19 -0500

    Add French language support to main function

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,17 @@ import { generatePrompt } from './prompt.js';
 import { fetchImage } from './image.js';
 import { sendEmail } from './email.js';
 import { sendTelegram } from './telegram.js';
-
+import { pickFrench } from './french.js';
 async function main() {
   const prompt = generatePrompt();
   console.log(`Prompt: ${prompt.text}`);
-
   const image = await fetchImage(prompt.keyword);
   console.log(`Image by ${image.photographerName}`);
-
-  await Promise.all([sendEmail(prompt, image), sendTelegram(prompt, image)]);
+  const french = pickFrench();
+  console.log(`French: ${french.fr}`);
+  await Promise.all([sendEmail(prompt, image, french), sendTelegram(prompt, image, french)]);
   console.log('Delivered.');
 }
-
 main().catch((err) => {
   console.error(err);
   process.exit(1);
